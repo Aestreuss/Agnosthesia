cc7dac36f139947e7e5f308aae3df18806434974
Aestreuss <euphyxia@gmail.com>
2024-08-27 18:21:35 +1200

    key image pop up

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,6 +6,7 @@ public class PlayerStats : MonoBehaviour
 {
     public int soulsCollected;
     public UIManager uiManager;
+    public int keyCollected;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +17,14 @@ public class PlayerStats : MonoBehaviour
             Destroy(collision.gameObject);
         }
         
+        if (collision.CompareTag("Key"))
+        {
+            Debug.Log("key collide");
+
+            keyCollected += 1;
+            uiManager.KeyFound(keyCollected);
+            Destroy(collision.gameObject);
+        }
     }
 
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI soulAmount_Text;
     public int soulAmount;
+    public GameObject keyImage;
+    public int key;
 
     public void UpdateSoulAmount(int soulsAmount)
     {
         soulAmount += 1;
         soulAmount_Text.text = "Souls: " + soulsAmount.ToString();       
+    }
+
+    public void KeyFound(int keyCollected)
+    {
+        key += 1;
+        keyImage.SetActive(true);
     }
 
     void Update()
